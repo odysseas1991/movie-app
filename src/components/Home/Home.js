55e6b60cd74a6e8bf1ff1f8577a95a74c98223a1
Odysseas Kouloukythas <odysseas.kouloukythas@gmail.com>
2022-04-04 22:41:32 +0300

    Added api call at homepage. Next step is the configuration of redux store.

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,8 +1,28 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import MovieListing from '../MovieListing/MovieListing'
+import movieApi from '../../common/apis/movieApi'
+import { APIKey } from '../../common/apis/MovieApiKey'
 
 const Home = () => {
+  useEffect(() => {
+    const movieText= "Harry";
+    const fetchMovies = async () => {
+      const response = await movieApi
+      .get(`?apikey=${APIKey}&s=${movieText}&type=movie`)
+      .catch((err)=>{
+        console.log("Error: ", err)
+      });
+      console.log("RESPONSE: ", response)
+    }
+
+    fetchMovies();
+  },[])
+
   return (
-    <div>Home</div>
+    <div>
+      <div className='banner-img'></div>
+      <MovieListing />
+    </div>
   )
 }
 
