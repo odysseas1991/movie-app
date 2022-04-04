1dd99d1674bd6d27d2c2cf6f45cae13497dbca51
Odysseas Kouloukythas <odysseas.kouloukythas@gmail.com>
2022-04-04 11:33:09 +0300

    Added routing. Switch syntax changed to Route.

--- src/App.js
+++ src/App.js
@@ -1,21 +1,24 @@
+import React from "react"
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./components/Home/Home"
+import Header from "./components/Header/Header"
+import Footer from "./components/Footer/Footer"
+import PageNotFound from "./components/PageNotFound/PageNotFound"
+import MovieDetail from "./components/MovieDetail/MovieDetail"
 import './App.scss';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <Router>
+        <Header></Header>
+        <Routes>
+          <Route path="/" element={<Home/>} />
+          <Route path="/movie/:imdbID" element={<MovieDetail/>} />
+          <Route element={<PageNotFound/>} />
+        </Routes>
+        <Footer />
+      </Router>
     </div>
   );
 }
