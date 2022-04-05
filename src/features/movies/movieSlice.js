9abc563ce5df8bbbd65b43165dc39789c7a37326
Odysseas Kouloukythas <odysseas.kouloukythas@gmail.com>
2022-04-05 16:32:41 +0300

    Changed the synchronous action creator to an async action creator using middleware thunk in redux toolkit.

--- src/features/movies/movieSlice.js
+++ src/features/movies/movieSlice.js
@@ -1,4 +1,18 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import movieApi from '../../common/apis/movieApi'
+import { APIKey } from '../../common/apis/MovieApiKey'
+
+export const fetchAsyncMovies = createAsyncThunk(
+    "movies/fetchAsyncMovies",
+    async () => {
+        const movieText= "Harry";
+        const response = await movieApi.get(
+            `?apikey=${APIKey}&s=${movieText}&type=movie`
+            )
+      
+      return response.data;
+    }
+)
 
 const initialState = {
     movies: {}
@@ -10,6 +24,18 @@ const movieSlice = createSlice({
     reducers: {
         addMovies: (state, { payload }) => {
             state.movies = payload
+        }
+    },
+    extraReducers: {
+        [fetchAsyncMovies.pending]: () => {
+            console.log("Pending")
+        },
+        [fetchAsyncMovies.fulfilled]: (state, { payload }) => {
+            console.log("Fetched Successfully")
+            return { ...state, movies: payload}
+        },
+        [fetchAsyncMovies.rejected]: () => {
+            console.log("Rejected")
         }
     }
 })
