d03b080ee7ee1d0772f9a058866e028b8ea73ed8
Odysseas Kouloukythas <odysseas.kouloukythas@gmail.com>
2022-04-10 15:11:23 +0300

    Added search bar and search logic!

--- src/features/movies/movieSlice.js
+++ src/features/movies/movieSlice.js
@@ -4,10 +4,10 @@ import { APIKey } from '../../common/apis/MovieApiKey'
 
 export const fetchAsyncMovies = createAsyncThunk(
     "movies/fetchAsyncMovies",
-    async () => {
-        const movieText= "Harry";
+    async (term) => {
+
         const response = await movieApi.get(
-            `?apikey=${APIKey}&s=${movieText}&type=movie`
+            `?apikey=${APIKey}&s=${term}&type=movie`
             )
       
       return response.data;
@@ -16,10 +16,10 @@ export const fetchAsyncMovies = createAsyncThunk(
 
 export const fetchAsyncShows = createAsyncThunk(
     "movies/fetchAsyncShows",
-    async () => {
-        const seriesText= "Friends";
+    async (term) => {
+
         const response = await movieApi.get(
-            `?apikey=${APIKey}&s=${seriesText}&type=series`
+            `?apikey=${APIKey}&s=${term}&type=series`
             )
       
       return response.data;
